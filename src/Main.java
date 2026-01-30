63f72bd997d9e5c2495aac5ec767a97a4467cf22
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-01-30 23:42:25 +0530

    updated the age calculator added local time and period to get current date and months

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-import java.util.*;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
 public class Main {
-    static void main(String[] args) {
-        Scanner input =new Scanner(System.in);
-        System.out.println("ENTER YOUR AGE");
-        int age = input.nextInt();
-        if (age >18){
-            System.out.println("you are adult");
-        } else if (age<18 && age >14) {
-            System.out.println("you are teenager");
-        }else {
-            System.out.println("you are kid");
-        }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your birth year (YYYY): ");
+        int year = sc.nextInt();
+        System.out.print("Enter your birth month (MM): ");
+        int month = sc.nextInt();
+        System.out.print("Enter your birth day (DD): ");
+        int day = sc.nextInt();
+        LocalDate birthDate = LocalDate.of(year, month, day);
+        LocalDate currentDate = LocalDate.now();
+        Period age = Period.between(birthDate, currentDate);
+        System.out.println("\nYour Age is:");
+        System.out.println(age.getYears() + " Years "
+                + age.getMonths() + " Months "
+                + age.getDays() + " Days");
+
+        sc.close();
     }
 }
