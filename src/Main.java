0bdf1dfd1819aac63114d88980042ba17ed4fced
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-01-30 23:08:12 +0530

    age group predicter

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
-
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import java.util.*;
+public class Main {
+    static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.println("ENTER YOUR AGE");
+        int age = input.nextInt();
+        if (age >18){
+            System.out.println("you are adult");
+        } else if (age<18 && age >14) {
+            System.out.println("you are teenager");
+        }else {
+            System.out.println("you are kid");
+        }
+    }
 }
